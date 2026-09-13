b86c09d8e787b31ab651c0de0dd6c655b9fb12f7
Sam <Sam.Arcanum@gmail.com>
2026-09-13 18:59:41 +0300

    chore(versions): регенерированный module_macros_version.h

--- module_macros_version.h
+++ module_macros_version.h
@@ -1,6 +1,6 @@
 // Auto-generated version file for module: module_macros
-// Generated: 2026-09-12 17:51
-// Environment: esp32_electronica7_rgb
+// Generated: 2026-09-13 13:19
+// Environment: esp32_electronica7_rgb_macros
 
 #ifndef MODULE_MACROS_VERSION_H
 #define MODULE_MACROS_VERSION_H
@@ -10,36 +10,36 @@
 // ============================================================
 
 // Числовая версия (для сравнений)
-#define MODULE_MACROS_VERSION 5
+#define MODULE_MACROS_VERSION 6
 
 // Строковая версия
-#define MODULE_MACROS_VERSION_STR "5"
+#define MODULE_MACROS_VERSION_STR "6"
 
 // Хэш последнего обработанного коммита кода (без учёта *_version.h)
-#define MODULE_MACROS_COMMIT_HASH "6f7444669e25348a2d2f035310b9dbe28bddea9c"
+#define MODULE_MACROS_COMMIT_HASH "b722cfc967414d4725074e98b6df492db6e2df70"
 
 // ============================================================
 // ДАТА ПОСЛЕДНЕГО ИЗМЕНЕНИЯ
 // ============================================================
 
 // Дата в формате yyyy.mm.dd hh.mm (как строка)
-#define MODULE_MACROS_COMMIT_DATE_STR "2026.09.12 00:51"
+#define MODULE_MACROS_COMMIT_DATE_STR "2026.09.13 00:53"
 
 // Компоненты даты (для числовых операций)
 #define MODULE_MACROS_COMMIT_YEAR 2026
 #define MODULE_MACROS_COMMIT_MONTH 9
-#define MODULE_MACROS_COMMIT_DAY 12
+#define MODULE_MACROS_COMMIT_DAY 13
 #define MODULE_MACROS_COMMIT_HOUR 0
-#define MODULE_MACROS_COMMIT_MINUTE 51
+#define MODULE_MACROS_COMMIT_MINUTE 53
 
 // Полная дата в формате YYYY-MM-DD HH:MM (для отладки)
-#define MODULE_MACROS_COMMIT_DATE "2026-09-12 00:51"
+#define MODULE_MACROS_COMMIT_DATE "2026-09-13 00:53"
 
 // ============================================================
 // ИНФОРМАЦИЯ О ГЕНЕРАЦИИ
 // ============================================================
 
-#define MODULE_MACROS_GENERATED_TIME "2026-09-12 17:51"
-#define MODULE_MACROS_GENERATED_TIMESTAMP "20260912_175117"
+#define MODULE_MACROS_GENERATED_TIME "2026-09-13 13:19"
+#define MODULE_MACROS_GENERATED_TIMESTAMP "20260913_131936"
 
 #endif // MODULE_MACROS_VERSION_H
